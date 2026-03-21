81e2af0ba9ce6688487b59ccb9ee9f376af6fc90
ceylog <ceylog@sina.com>
2026-03-22 03:43:21 +0800

    update

--- scripts/cust_packages.sh
+++ scripts/cust_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
+echo 'src-git aurora https://github.com/eamonxg/luci-theme-aurora.git' >> feeds.conf.default
+echo 'src-git aurora-config https://github.com/eamonxg/luci-app-aurora-config.git' >> feeds.conf.default
