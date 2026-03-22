f6d97b21d8b646ba5c68261457e6065beed32205
ceylog <ceylog@sina.com>
2026-03-22 14:06:51 +0800

    update

--- scripts/cust_init_settings.sh
+++ scripts/cust_init_settings.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.1.254/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-aurora/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/gwrt/g' package/base-files/files/bin/config_generate
 
 # add date in output file name
 sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
--- scripts/cust_packages.sh
+++ scripts/cust_packages.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo 'src-git theme_aurora https://github.com/eamonxg/luci-theme-aurora.git;master' >> feeds.conf.default
-echo 'src-git aurora_config https://github.com/eamonxg/luci-app-aurora-config.git;master' >> feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
