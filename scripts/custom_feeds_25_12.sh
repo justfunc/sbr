fab3423e9f7a50b502789fab1f696cc2e50d12ce
ceylog <ceylog@sina.com>
2026-03-26 23:23:10 +0800

    chore: 移除aurora主题和kenzo源相关配置

--- scripts/custom_feeds_25_12.sh
+++ scripts/custom_feeds_25_12.sh
@@ -5,8 +5,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 rm -rf package/luci-theme-argon
