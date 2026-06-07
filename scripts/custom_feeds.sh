51cbbf7107b81ebabd2270571512979e18ae1f02
ceylog <ceylog@sina.com>
2026-06-07 12:08:43 +0800

    add daed

--- scripts/custom_feeds.sh
+++ scripts/custom_feeds.sh
@@ -24,23 +24,25 @@ git clone --depth 1 --single-branch https://github.com/sbwml/luci-theme-argon pa
 
 
 # 常用工具与应用
-rm -rf package/netspeedtest
-git clone --depth 1 --single-branch https://github.com/sirpdboy/netspeedtest package/netspeedtest  #homebox speedtest测速
-rm -rf package/luci-app-poweroffdevice
-git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice   #关机
-rm -rf package/luci-app-taskplan
-git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan    #计划任务
+# rm -rf package/netspeedtest
+# git clone --depth 1 --single-branch https://github.com/sirpdboy/netspeedtest package/netspeedtest  #homebox speedtest测速
+# rm -rf package/luci-app-poweroffdevice
+# git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice   #关机
+# rm -rf package/luci-app-taskplan
+# git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan    #计划任务
 # rm -rf package/luci-app-advancedplus
 # git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus  #高级设置
 # rm -rf package/luci-app-authshield 
 # git clone --depth 1 --single-branch https://github.com/iv7777/luci-app-authshield package/luci-app-authshield  #防止异常登录保护
-rm -rf package/luci-app-timewol
-git clone --depth 1 --single-branch https://github.com/VIKINGYFY/packages package/luci-app-timewol
-rm -rf package/luci-app-wolplus
-git clone --depth 1 --single-branch https://github.com/VIKINGYFY/packages package/luci-app-wolplus
-rm -rf package/luci-app-owq-wol
-git clone --depth 1 --single-branch https://github.com/isalikai/luci-app-owq-wol package/luci-app-owq-wol  # wol加强版
+# rm -rf package/luci-app-timewol
+# git clone --depth 1 --single-branch https://github.com/VIKINGYFY/packages package/luci-app-timewol
+# rm -rf package/luci-app-wolplus
+# git clone --depth 1 --single-branch https://github.com/VIKINGYFY/packages package/luci-app-wolplus
+# rm -rf package/luci-app-owq-wol
+# git clone --depth 1 --single-branch https://github.com/isalikai/luci-app-owq-wol package/luci-app-owq-wol  # wol加强版
 # rm -rf package/luci-app-partexp
 # git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
 # rm -rf package/luci-app-quickfile
 # git clone --depth 1 --single-branch https://github.com/sbwml/luci-app-quickfile package/luci-app-quickfile
+rm -rf package/dae
+git clone --depth 1 --single-branch https://github.com/QiuSimons/luci-app-daed package/dae
